a391d79a4774519b29bc06bcd6099bac40fbda4e
sushil2601 <sushilsuman2355@gmail.com>
2024-12-02 18:00:52 +0530

    Routing and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import NavBar from "./NavBar"
+import { BrowserRouter ,Routes,Route} from "react-router"
+import Body from "./Body"
+import Login from "./Login"
+import Profile from "./Profile"
 
 function App() {
   
   return (
     <>
-      <NavBar />
-      <h1 className="text-3xl font-bold">
-        Hello world!
-      </h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />}/>
+            <Route path="/profile" element ={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      
     </>
   )
 }
